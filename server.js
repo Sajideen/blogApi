fc68d2b826d04264b6d2c9fd746a89197572cfa2
Sajideen <muhammad.bscs3297@iiu.edu.pk>
2021-02-03 11:58:14 +0500

    commit

--- server.js
+++ server.js
@@ -48,8 +48,3 @@ require("./app/routes/blog.routes.js")(app);
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
-
-// listen for requests
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
